ef7672f63b1e7e0ab5fac1ed64e42aefa9e8d107
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-06-13 23:08:25 +0530

    :sparkles: feat: bookmarks and liked posts pages

---
+++ src/components/features/liked-posts/LikedPosts.jsx
@@ -0,0 +1,7 @@
+import "./LikedPosts.css";
+
+const LikedPosts = () => {
+  return <div>LikedPosts</div>;
+};
+
+export default LikedPosts;
---
+++ src/pages/bookmarked-posts-page/BookmarkedPostsPage.jsx
@@ -0,0 +1,7 @@
+import BookmarkedPosts from "components/features/bookmarked-posts/BookmarkedPosts";
+
+const BookmarkedPostsPage = () => {
+  return <BookmarkedPosts />;
+};
+
+export default BookmarkedPostsPage;
