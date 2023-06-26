87b0f2af65ea77f40bbddecb55e1302841af7ddf
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-06-26 20:19:43 +0530

    :construction: updated: posts context and reducer

--- src/core/app-routes/AppRoutes.js
+++ src/core/app-routes/AppRoutes.js
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 import LoginPage from "pages/login-page/LoginPage";
 import SignupPage from "pages/signup-page/SignupPage";
-import FollowingFeedPage from "pages/following-feed-page/FollowingFeedPage";
+import HomeFeedPage from "pages/home-feed-page/HomeFeedPage";
 import ExploreFeedPage from "pages/explore-feed-page/ExploreFeedPage";
 import { AuthGuard } from "core/auth-guard/AuthGuard";
 
@@ -11,7 +11,7 @@ const AppRoutes = () => {
   const protectedRoutes = [
     {
       path: "/",
-      component: FollowingFeedPage,
+      component: HomeFeedPage,
     },
     {
       path: "/explore",
