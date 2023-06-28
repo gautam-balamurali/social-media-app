9fe6a080d8e862d128e076a0fbc5712a562314cd
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-06-28 17:03:35 +0530

    :construction: update: app layout

--- src/components/layouts/app-layout/AppLayout.jsx
+++ src/components/layouts/app-layout/AppLayout.jsx
@@ -1,10 +1,26 @@
+import { useLocation } from "react-router-dom";
+
 import AppRoutes from "core/app-routes/AppRoutes";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+import "./AppLayout.css";
+import Navbar from "../navbar/Navbar";
 
 const AppLayout = () => {
+  const location = useLocation();
+
+  const onAuthenticationPage = () =>
+    location.pathname === "/login" || location.pathname === "/sign-up";
+
   return (
-    <section className="section">
-      <AppRoutes />
-    </section>
+    <div className="app-layout">
+      {!onAuthenticationPage() && <Header />}
+      <section className="section">
+        <AppRoutes />
+      </section>
+      <Footer />
+      <Navbar />
+    </div>
   );
 };
 
