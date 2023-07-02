0dab19aebf5bfcd96026cfae533e29afb9893a19
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-07-02 17:13:46 +0530

    :boom: API integration for follow unfollow user

--- src/core/contexts/users-context/UsersContext.js
+++ src/core/contexts/users-context/UsersContext.js
@@ -3,6 +3,11 @@ import { createContext, useContext, useEffect, useReducer } from "react";
 import { usersReducer } from "core/reducers/users-reducer/UsersReducerFunction";
 import { usersReducerInitialState } from "core/reducers/users-reducer/UsersReducerInitialState";
 import { getAllUsersService } from "core/services/user-service/user.service";
+import {
+  followUserService,
+  unfollowUserService,
+} from "core/services/follow-unfollow-service/followUnfollow.service";
+import { useAuthentication } from "../authentication-context/AuthenticationContext";
 
 export const UsersContext = createContext();
 
@@ -11,6 +16,8 @@ export const UsersProvider = ({ children }) => {
     usersReducer,
     usersReducerInitialState
   );
+
+  const { token, syncUserDetails } = useAuthentication();
 
   const fetchAllUsers = async () => {
     try {
@@ -26,12 +33,58 @@ export const UsersProvider = ({ children }) => {
     }
   };
 
+  const followUser = async (followUserId) => {
+    try {
+      const response = await followUserService(followUserId, token);
+      console.log({ response }, "followUser");
+      if (response.status === 200 || response.status === 201) {
+        const { followUser, user } = response?.data;
+        const updatedUsersList = state.users.map(
+          (userDetails) =>
+            [followUser, user].find(({ _id }) => _id === userDetails._id) ||
+            userDetails
+        );
+        usersDispatch({
+          type: "UPDATE_USERS",
+          payload: updatedUsersList,
+        });
+        syncUserDetails(token, user);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const unfollowUser = async (followUserId) => {
+    try {
+      const response = await unfollowUserService(followUserId, token);
+      console.log({ response }, "unfollowUser");
+      if (response.status === 200 || response.status === 201) {
+        const { followUser, user } = response?.data;
+        const updatedUsersList = state.users.map(
+          (userDetails) =>
+            [followUser, user].find(({ _id }) => _id === userDetails._id) ||
+            userDetails
+        );
+        usersDispatch({
+          type: "UPDATE_USERS",
+          payload: updatedUsersList,
+        });
+        syncUserDetails(token, user);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     fetchAllUsers();
   }, []);
 
   return (
-    <UsersContext.Provider value={{ ...state, usersDispatch }}>
+    <UsersContext.Provider
+      value={{ ...state, usersDispatch, followUser, unfollowUser }}
+    >
       {children}
     </UsersContext.Provider>
   );
