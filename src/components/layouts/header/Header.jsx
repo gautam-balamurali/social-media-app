c762dd18496841ebd4f6cf9a26d37a96331afb0c
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-07-10 10:41:10 +0530

    :boom: feat: app logo

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -3,7 +3,12 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="header">
-      <h1>Sochenge</h1>
+      <img
+        className="App-logo"
+        src="https://res.cloudinary.com/dbe8yf165/image/upload/v1688964194/twitlyx/logo-2_ihpmf7.png"
+        alt="twitlyx-logo"
+      />
+      <h1>TwitLyx</h1>
     </header>
   );
 };
